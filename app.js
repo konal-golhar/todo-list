c169455f6b8e019bdaa60843bcd5f926a2b42003
konal <konalgolhar@gmail.com>
2022-03-12 17:13:12 +0530

    version1 complete

---
+++ app.js
@@ -0,0 +1,40 @@
+const express=require("express");
+
+const app=express();
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set('view engine','ejs');
+var items=[];
+
+app.get("/",function(req,res){
+var today=new Date();
+var option={
+weekday:"long",
+day:"numeric",
+month:"long"
+};
+var day=today.toLocaleDateString("en-US",option);
+
+res.render("list",{newdate:day ,newitem:items});
+
+});
+app.post("/" ,function(req,res){
+ item=req.body.list;
+ items.push(item);
+ console.log(items[i]);
+
+res.redirect("/");
+
+
+
+})
+
+
+
+
+
+app.listen(process.env.PORT || 3000,function(req,res){
+console.log("running at port 3000");
+
+})
